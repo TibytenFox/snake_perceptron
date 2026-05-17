7e32ecd214a5f589fa6d3f2e27bbd622dfddf46e
TibytenFox <andrew.kutin@gmail.com>
2026-05-17 03:53:18 +0300

    refactor: improve Matrix and Perceptron classes with additional methods and formatting

--- src/Matrix.py
+++ src/Matrix.py
@@ -3,22 +3,22 @@ import random
 
 
 def sigmoid(x):
-	return 1 / (1 + math.e ** (-x))
+    return 1 / (1 + math.e ** (-x))
 
-def column(arr):
-	res = Matrix(len(arr), 1)
-	for i in range(len(arr)):
-		res.matrix[i][0] = arr[i]
-	
-	return arr
+def column(arr: list):
+    res = Matrix(len(arr), 1)
+    for i in range(len(arr)):
+        res.matrix[i][0] = arr[i]
+
+    return arr
 
 class Matrix:
-	rows: int
-	cols: int
-	data: [[float]]
+    rows: int
+    cols: int
+    data: [[float]]
 
-	def __init__(self, *args, **kwargs):
-		if "data" in kwargs:
+    def __init__(self, *args, **kwargs):
+        if "data" in kwargs:
             self.rows = len(kwargs["data"])
             self.cols = len(kwargs["data"][0])
             self.matrix = kwargs["data"].copy()
@@ -27,80 +27,115 @@ class Matrix:
         self.cols = args[1]
         self.matrix = [[0 for _ in range(self.cols)] for __ in range(self.rows)]
 
-	def fromArray(self, arr):
-		for i in range(self.rows):
-			for j in range(self.cols):
-				self.matrix[i][j] = arr[i * self.cols + j]
+    def fromArray(self, arr: list):
+        for i in range(self.rows):
+            for j in range(self.cols):
+                self.matrix[i][j] = arr[i * self.cols + j]
 
-	def toArray(self):
-		res = []
-		for i in range(self.rows):
-			for j in range(self.cols):
-				res.append(self.matrix[i][j])
+    def toArray(self):
+        res = []
+        for i in range(self.rows):
+            for j in range(self.cols):
+                res.append(self.matrix[i][j])
 
-	def randomize(self):
-		for i in range(self.rows):
-			for j in range(self.cols):
-				self.matrix[i][j] = random.uniform(-1, 1)
+        return res
 
-	def __mul__(self, other):
-		if isinstance(other, Matrix):
-			if not (self.cols == other.rows): return 
-			res = Matrix(self.rows, other.cols)
+    def randomize(self):
+        for i in range(self.rows):
+            for j in range(self.cols):
+                self.matrix[i][j] = random.uniform(-1, 1)
 
-			for i in range(self.cols):
-				for j in range(other.rows):
-					for ind in range(self.cols):
-						res.matrx[i][j] += self.matrix[i + ind] * other.matrix[j + ind]
+    def addBias(self):
+        res = Matrix(self.rows + 1, 1)
+        for i in range(self.rows):
+            res.matrix[i][0] = self.matrix[i][0]
+        res.matrix[self.rows][0] = 1
 
-			return new_matrix
+        return res
 
-		res = Matrix(self.rows, self.cols)
-		for i in range(self.rows):
-			for j in range(self.cols):
-				res.matrix[i][j] = self.matrix[i][j] * other
+    def __mul__(self, other):
+        if isinstance(other, Matrix):
+            if not (self.cols == other.rows): return
+            res = Matrix(self.rows, other.cols)
 
-		return res
+            for i in range(self.cols):
+                for j in range(other.rows):
+                    for ind in range(self.cols):
+                        res.matrx[i][j] += self.matrix[i + ind] * other.matrix[j + ind]
 
-	def __add__(self, other):
-		if isinstance(other, Matrix):
+            return res
+
+        res = Matrix(self.rows, self.cols)
+        for i in range(self.rows):
+            for j in range(self.cols):
+                res.matrix[i][j] = self.matrix[i][j] * other
+
+        return res
+
+    def __add__(self, other):
+        if isinstance(other, Matrix):
             if not (self.rows == other.rows and self.cols == other.cols): return 
-			
-			res = Matrix(self.rows, self.cols)
-			for i in range(self.rows):
-				for j in range(self.cols):
-					res.matrix[i][j] = self.matrix[i][j] + other.matrix[i][j]
 
-			return res
+            res = Matrix(self.rows, self.cols)
+            for i in range(self.rows):
+                for j in range(self.cols):
+                    res.matrix[i][j] = self.matrix[i][j] + other.matrix[i][j]
 
-	def __sub__(self, other):
-		if isinstance(other, Matrix):
+            return res
+
+    def __sub__(self, other):
+        if isinstance(other, Matrix):
             if not (self.rows == other.rows and self.cols == other.cols): return 
-			
-			res = Matrix(self.rows, self.cols)
-			for i in range(self.rows):
-				for j in range(self.cols):
-					res.matrix[i][j] = self.matrix[i][j] - other.matrix[i][j]
 
-			return new_matrix
+            res = Matrix(self.rows, self.cols)
+            for i in range(self.rows):
+                for j in range(self.cols):
+                    res.matrix[i][j] = self.matrix[i][j] - other.matrix[i][j]
 
-	def activate(self):
-		res = Matrix(self.rows, self.cols)
-		for i in range(self.rows):
-			for j in range(self.cols):
-				res.matrix[i][j] = sigmoid(self.matrix[i][j])
+            return res
 
-		return res
+    def activate(self):
+        res = Matrix(self.rows, self.cols)
+        for i in range(self.rows):
+            for j in range(self.cols):
+                res.matrix[i][j] = sigmoid(self.matrix[i][j])
 
-	def transpose(self):
-		res = Matrix(self.cols, self.rows)
-		for i in range(self.rows):
-			for j in range(self.cols):
-				res.matrix[j][i] = self.matrix[i][j]
+        return res
 
-		return new_matrix
+    def mutate(self, mutation_rate):
+        for i in range(self.rows):
+            for j in range(self.cols):
+                random_num = random.random()
+                if random_num < mutation_rate:
+                    self.matrix[i][j] += random.gauss() / 5 # очень маленькое число
 
-	def clone(self):
+                    if self.matrix[i][j] < -1: self.matrix[i][j] = -1
+                    if self.matrix[i][j] > 1: self.matrix[i][j] = 1
+
+    def crossover(self, partner):
+        child = Matrix(self.rows, self.cols)
+
+        random_row = random.randint(0, self.rows - 1)
+        random_col = random.randint(0, self.cols - 1)
+
+        for i in range(self.rows):
+            for j in range(self.cols):
+                if i < random_row or (i == random_col and j <= random_col):
+                    child.matrix[i][j] = self.matrix[i][j]
+                else:
+                    child.matrix[i][j] = partner.matrix[i][j]
+
+        return child
+
+    def transpose(self):
+        res = Matrix(self.cols, self.rows)
+        for i in range(self.rows):
+            for j in range(self.cols):
+                res.matrix[j][i] = self.matrix[i][j]
+
+        return res
+
+    def clone(self):
         res = Matrix(self.rows, self.cols)
 
         for i in range(self.rows):
@@ -110,4 +145,3 @@ class Matrix:
         return res
 
 
-	
--- src/Perceptron.py
+++ src/Perceptron.py
@@ -3,11 +3,62 @@ from Matrix import *
 class Perceptron:
 	input_nodes: int
 	hidden_nodes: int
-	output_nodex: int
+	output_nodes: int
 
-	weigths_ih: Matrix
-	weights_hh: Matrix
-	weights_ho: Matrix
+	# Веса
+	whi: Matrix
+	whh: Matrix
+	woh: Matrix
 
-	def __init__(self, input, hidden, output):
-		
+	def __init__(self, input_count, hidden_count, output_count):
+		self.input_nodes = input_count
+		self.hidden_nodes = hidden_count
+		self.output_nodes = output_count
+
+		# +1 для bias
+		self.whi = Matrix(self.hidden_nodes, self.input_nodes + 1)
+		self.whh = Matrix(self.hidden_nodes, self.hidden_nodes + 1)
+		self.woh = Matrix(self.output_nodes, self.hidden_nodes + 1)
+
+		self.whi.randomize()
+		self.whh.randomize()
+		self.woh.randomize()
+
+	def mutate(self, mutation_rate):
+		self.whi.mutate(mutation_rate)
+		self.whh.mutate(mutation_rate)
+		self.woh.mutate(mutation_rate)
+
+	def output(self, input_arr):
+		inputs = column(input_arr)
+		input_bias = inputs.addBias()
+
+		hidden_inputs = self.whi * input_bias
+		hidden_outputs = hidden_inputs.activate()
+		hidden_outputs_bias = hidden_outputs.addBias()
+
+		hidden_inputs2 = self.whh * hidden_outputs_bias
+		hidden_outputs2 = hidden_inputs2.activate()
+		hidden_outputs_bias2 = hidden_outputs2.addBias()
+
+		output_inputs = self.woh * hidden_outputs_bias2
+		outputs = output_inputs.activate()
+
+		return outputs.toArray()
+
+	def crossover(self, partner):
+		child = Perceptron(self.input_nodes, self.hidden_nodes, self.output_nodes)
+
+		child.whi = self.whi.crossover(partner.whi)
+		child.whh = self.whh.crossover(partner.whh)
+		child.woh = self.woh.crossover(partner.woh)
+
+		return child
+
+	def clone(self):
+		res = Perceptron(self.input_nodes, self.hidden_nodes, self.output_nodes)
+
+		res.whi = self.whi.clone()
+		res.whh = self.whh.clone()
+		res.woh = self.woh.clone()
+
